71c5ecaf41aa6de18dc1872ac5a4b1acb9e879a6
jcmookat <jscardenasjr@gmail.com>
2021-06-09 11:39:06 +0900

    GUI listens to param changes

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -27,11 +27,22 @@ highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlope
     {
         addAndMakeVisible(comp);
     }
+    const auto& params = audioProcessor.getParameters();
+    for ( auto param : params) {
+        param->addListener(this);
+    }
+    
+    startTimer(60);
+    
     setSize (600, 400);
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 {
+    const auto& params = audioProcessor.getParameters();
+    for ( auto param : params) {
+        param->removeListener(this);
+    }
 }
 
 //==============================================================================
@@ -139,8 +150,11 @@ void SimpleEQAudioProcessorEditor::timerCallback()
     if ( parametersChanged.compareAndSetBool(false, true) )
     {
         //update the monochain
-        
+        auto chainSettings = getChainSettings(audioProcessor.apvts);
+        auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+        updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
         //signal a repaint
+        repaint();
     }
 }
 
